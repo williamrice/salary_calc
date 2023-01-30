a26d7198bcca41a2ebf4ef19bcb5434608563096
William Rice <billyrice12@live.com>
2023-01-29 21:48:31 -0500

    fixed some issues

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 const patrol_pay = 16.0;
-const sgt_pay = 18.0;
-const lt_pay = 20.0;
+const sgt_pay = 17.5;
+const lt_pay = 19.0;
 const chief_pay = 22.0;
 
-const reg_budget_patrol = patrol_pay * 32 * 52 * 3;
+
+const weekly_reg_hours = 34;
+const abc_reg_hours = 6;
+const weekly_ot_hours = 4;
+const abc_ot_hours = 8;
+
+const reg_budget_patrol = patrol_pay * weekly_reg_hours * 52 * 2;
 const reg_budget_patrol_retire = reg_budget_patrol * 0.49;
 
-const abc_budget_patrol = patrol_pay * 8 * 52 * 3;
+const abc_budget_patrol = patrol_pay * abc_reg_hours * 52 * 2;
 const abc_budget_patrol_retire = abc_budget_patrol * 0.49;
 
-const reg_budget_sgt = sgt_pay * 32 * 52 * 1;
+const reg_budget_sgt = sgt_pay * weekly_reg_hours * 52 * 2;
 const reg_budget_sgt_retire = reg_budget_sgt * 0.49;
 
-const abc_budget_sgt = sgt_pay * 8 * 52 * 1;
+const abc_budget_sgt = sgt_pay * abc_reg_hours * 52 * 2;
 const abc_budget_sgt_retire = abc_budget_sgt * 0.49;
 
-const reg_budget_lt = lt_pay * 32 * 52 * 1;
+const reg_budget_lt = lt_pay * weekly_reg_hours * 52 * 1;
 const reg_budget_lt_retire = reg_budget_lt * 0.49;
 
-const abc_budget_lt = lt_pay * 8 * 52 * 1;
+const abc_budget_lt = lt_pay * abc_reg_hours * 52 * 1;
 const abc_budget_lt_retire = abc_budget_lt * 0.49;
 
-const reg_budget_chief = chief_pay * 32 * 52 * 1;
+const reg_budget_chief = chief_pay * weekly_reg_hours * 52 * 1;
 const reg_budget_chief_retire = reg_budget_chief * 0.49;
 
-const abc_budget_chief = chief_pay * 8 * 52 * 1;
+const abc_budget_chief = chief_pay * abc_reg_hours * 52 * 1;
 const abc_budget_chief_retire = abc_budget_chief * 0.49;
 
-const patrol_overtime_reg = patrol_pay * 1.5 * 6 * 26;
+const patrol_overtime_reg = patrol_pay * 1.5 * weekly_ot_hours * 26 * 2;
 const patrol_overtime_retire = patrol_overtime_reg * 0.49;
-const patrol_overtime_abc = patrol_pay * 1.5 * 6 * 26;
+const patrol_overtime_abc = patrol_pay * 1.5 * abc_ot_hours * 26 * 2;
 const patrol_overtime_abc_retire = patrol_overtime_abc * 0.49;
 
-const sgt_overtime_reg = sgt_pay * 1.5 * 6 * 26;
+const sgt_overtime_reg = sgt_pay * 1.5 * weekly_ot_hours * 26 * 2;
 const sgt_overtime_retire = sgt_overtime_reg * 0.49;
-const sgt_overtime_abc = sgt_pay * 1.5 * 6 * 26;
+const sgt_overtime_abc = sgt_pay * 1.5 * abc_ot_hours * 26 * 2;
 const sgt_overtime_abc_retire = sgt_overtime_abc * 0.49;
 
 const pd_regular_annual =
@@ -74,10 +80,21 @@ const patrol_base_pay =
   (reg_budget_patrol +
     abc_budget_patrol +
     patrol_overtime_reg +
-    patrol_overtime_abc) /
-  3;
+    patrol_overtime_abc +
+    4600.0) /
+  2;
+const sgt_base_pay =
+  (reg_budget_sgt +
+    abc_budget_sgt +
+    sgt_overtime_reg +
+    sgt_overtime_abc +
+    4600) /
+  2;
 
-console.log(`Total regular budget salary -- ${pd_regular_annual}\n`);
+const lt_base_pay = reg_budget_lt + abc_budget_lt + 4600;
+const chief_base_pay = reg_budget_chief + abc_budget_chief + 4600;
+
+console.log(`Total regular budget salary -- ${pd_regular_annual.toFixed(2)}\n`);
 console.log(
   `Total regular budget retirement -- ${pd_regular_annual_retire}\n\n`
 );
@@ -97,5 +114,8 @@ console.log(
 console.log(`Total abc budget -- ${total_abc}\n\n`);
 
 console.log(`Total regular budget -- ${total_reg_budget}\n\n`);
-console.log(`Total regular budget retire -- ${total_reg_budget_retire}\n\n`);
+console.log(`Total regular budget retire -- ${total_reg_budget_retire.toFixed(2)}\n\n`);
 console.log(`Total Patrol base pay -- ${patrol_base_pay}\n\n`);
+console.log(`Total sgt base pay -- ${sgt_base_pay}\n\n`);
+console.log(`Total lt base pay -- ${lt_base_pay}\n\n`);
+console.log(`Total chief base pay -- ${chief_base_pay}\n\n`);
